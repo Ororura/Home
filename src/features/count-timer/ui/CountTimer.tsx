6bbb3bcd37c93fb09c2c2e9cf6c29fc30c9ee8f8
Ororura <hitmanlol890@gmail.com>
2025-12-06 22:56:40 +0300

    update timer ui

--- src/features/count-timer/ui/CountTimer.tsx
+++ src/features/count-timer/ui/CountTimer.tsx
@@ -7,31 +7,14 @@ export const CountTimer: FC = () => {
   const { days, hours, minutes, seconds } = useCountDown();
 
   return (
-    <div className="w-full flex justify-center mt-10">
-      <div className="bg-white/10 backdrop-blur-xl border border-white/20 rounded-2xl p-8 shadow-xl text-center">
-        <h2 className="text-2xl font-semibold mb-6">Дней до дома</h2>
-
-        <div className="grid grid-cols-4 gap-4 ">
-          <div className="flex flex-col items-center bg-white/5 p-4 rounded-xl">
-            <span className="text-4xl font-bold">{days}</span>
-            <span className="text-sm opacity-70">Дней</span>
-          </div>
-
-          <div className="flex flex-col items-center bg-white/5 p-4 rounded-xl">
-            <span className="text-4xl font-bold">{hours}</span>
-            <span className="text-sm opacity-70">Часов</span>
-          </div>
-
-          <div className="flex flex-col items-center bg-white/5 p-4 rounded-xl">
-            <span className="text-4xl font-bold">{minutes}</span>
-            <span className="text-sm opacity-70">Минут</span>
-          </div>
-
-          <div className="flex flex-col items-center bg-white/5 p-4 rounded-xl">
-            <span className="text-4xl font-bold">{seconds}</span>
-            <span className="text-sm opacity-70">Секунд</span>
-          </div>
+    <div className="w-full mt-1.5">
+      <div className="mx-auto w-fit">
+        <h1>Когда? Когда? Когда?</h1>
+        <div>
+          <p>Дней этак через... {days}</p>
         </div>
+        <p>А секунд?</p>
+        <p>Что-то около {seconds} секунд</p>
       </div>
     </div>
   );
